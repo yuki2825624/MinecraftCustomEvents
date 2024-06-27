de443e07a7de8d9a5e4875742562b28063cbd27d
yuki2825624 <122198686+yuki2825624@users.noreply.github.com>
2024-06-27 22:21:37 +0900

    Provide examples.

---
+++ example.js
@@ -0,0 +1,35 @@
+import { playerJumpEvent } from "./scripts/player/jump";
+import { playerLandEvent } from "./scripts/player/land";
+import { playerMoveEvent } from "./scripts/player/move";
+import { tickEvent } from "./scripts/tick";
+
+// TickEvent
+tickEvent.subscribe((ev) => {
+    const { tps } = ev;
+    if (tps < 10) {
+        console.warn("TPSが極端に低下しています。");
+    }
+});
+
+// MoveEvent
+playerMoveEvent.subscribe((ev) => {
+    const { player } = ev;
+    if (!player.hasTag("movable")) {
+        ev.cancel = true;
+        player.sendMessage("移動が許可されていません。");
+    }
+});
+
+// JumpEvent
+playerJumpEvent.subscribe((ev) => {
+    const { player } = ev;
+    if (!player.isOnGround) {
+        player.applyKnockback(0, 0, 0, 1);
+    }
+});
+
+// LandEvent
+playerLandEvent.subscribe((ev) => {
+    const { player, fallDistance } = ev;
+    player.sendMessage(`${fallDistance.toFixed(2)}ブロック落下しました。`);
+});
